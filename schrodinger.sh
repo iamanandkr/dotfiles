6aff6d51592e6c478fc7cfbe450e8b24ea8441ad
Anand Kumar <iamanandkr95@gmail.com>
2022-06-14 20:28:57 +0530

    Alias updates.

--- schrodinger.sh
+++ schrodinger.sh
@@ -6,19 +6,21 @@ function sdgr() {
     branch=$1;
     export SCHRODINGER_SRC=/Users/$USER/builds/$branch/source;
     export SCHRODINGER=/Users/$USER/builds/$branch/build;
-    source $SCHRODINGER_SRC/mmshare/build_env -b;
+    # source $SCHRODINGER_SRC/mmshare/build_env -b;
 }
-alias 22-2='sdgr 2022-2'
+alias 22-3='sdgr 2022-3'
 
+alias src='cd $SCHRODINGER_SRC'
+alias ssch='source $SCHRODINGER_SRC/mmshare/build_env -b'
 alias mm='cd ${SCHRODINGER_SRC}/mmshare'
-alias sdgr_build='cd $SCHRODINGER'
+alias bld='cd $SCHRODINGER'
 alias mm_build='cd ${SCHRODINGER}/mmshare-v*'
 alias mm_test='cd ${SCHRODINGER}/mmshare-v*/python/test'
-alias schrun='${SCHRODINGER}/run'
+alias srun='${SCHRODINGER}/run'
 alias buildinger='${SCHRODINGER_SRC}/mmshare/build_tools/buildinger.sh'
 
-alias pytest='${SCHRODINGER}/utilities/py.test'
-alias mpytest='make python && pytest'
+alias spytest='srun py.test'
+alias mpytest='make python && spytest'
 
 alias maestro='${SCHRODINGER}/maestro -console'
 
@@ -42,3 +44,7 @@ function switch_all_branch() {
 
 alias maelog='less $SCHRODINGER/maestro-v*/make_maestro-src_all.log'
 alias mmlog='less $SCHRODINGER/mmshare-v*/make_mmshare_all.log'
+
+
+alias mps='make python-scripts'
+alias mpm='make python-modules'
